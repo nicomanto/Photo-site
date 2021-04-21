67f0ef516d3f1278d59cb00f3ae58316e4a784dc
Niccolò Mantovani <nicomanto49@gmail.com>
2021-04-21 13:34:30 +0200

    Add 404 on folder not found

--- pages/photoListFolder/[folder].tsx
+++ pages/photoListFolder/[folder].tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next";
+import Error from "next/error";
 import Layout from "../../components/Layout";
 import PhotoList from "../../components/Photo/PhotoList";
 import { getPhotoList } from "../../server/Cloudinary/managePhoto";
@@ -7,24 +8,39 @@ import Photo from "../../interfaces/Photo";
 type Props = {
   photoList: Photo[];
   folderName: string;
+  errorCode: boolean;
 };
 
-const GalleryPage = ({ photoList, folderName }: Props) => (
-  <Layout title={`Home | ${folderName} Gallery`}>
-    <h1>
-      Galleria di
-      {` ${folderName}`}
-    </h1>
-    <PhotoList items={photoList} />
-  </Layout>
-);
+const GalleryPage = ({ errorCode, photoList, folderName }: Props) => {
+  if (errorCode) {
+    return <Error statusCode={404} />;
+  }
+
+  return (
+    <Layout title={`Home | ${folderName} Gallery`}>
+      <h1>
+        Galleria di
+        {` ${folderName}`}
+      </h1>
+      <PhotoList items={photoList} />
+    </Layout>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const folder: string = params!.folder.toString();
+  const photoList: Photo[] = await getPhotoList(folder);
+  let errorCode = false;
+
+  if (photoList.length === 0) {
+    errorCode = true;
+  }
+
   return {
     props: {
-      photoList: await getPhotoList(folder),
+      photoList,
       folderName: folder,
+      errorCode,
     },
   };
 };
