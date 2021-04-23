eee6a0617f3418844300e72e7173f6bcdd931c50
Niccolò Mantovani <nicomanto49@gmail.com>
2021-04-23 10:13:41 +0200

    Test 2

--- pages/api/contact/index.tsx
+++ pages/api/contact/index.tsx
@@ -1,7 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest} from "next";
 import sendEmail from "../../../server/Nodemailer/manageEmail";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest) => {
   console.log(req.body);
   console.log(await sendEmail(req.body));
 };
