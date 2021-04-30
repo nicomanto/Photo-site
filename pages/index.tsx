be5c39f1d26af45fcaafa1d1f3e3136b90460f3f
Niccolò Mantovani <nicomanto49@gmail.com>
2021-05-01 00:58:35 +0200

    Maybe fix orizontal scroll in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,23 +17,25 @@ const IndexPage = ({ title, subtitle, citation, photos }: Props) => (
       {title}
     </h1>
     <h2 className="display-4 mx-2 text-center subtitle-home">{subtitle}</h2>
-    <div className="row text-center py-5 px-5">
-      <Image
-        cloudName="dszun6oiu"
-        className="photoPresentation col-md-4 py-2"
-        alt={photos[0].name}
-        publicId={photos[0].publicId}
-        width="500"
-        height="500"
-        crop="fill"
-        loading="lazy"
-      />
-      <blockquote
-        className="my-auto col-md-8"
-        cite="https://www.lefrasi.com/frase/victor-hugo-quasi-tutto-segreto-delle-anime-grandi?bg=p42"
-      >
-        <p lang="it" className="citation">{`"${citation}"`}</p>
-      </blockquote>
+    <div className="py-5 px-5">
+      <div className="row text-center">
+        <Image
+          cloudName="dszun6oiu"
+          className="photoPresentation col-md-4 py-2"
+          alt={photos[0].name}
+          publicId={photos[0].publicId}
+          width="500"
+          height="500"
+          crop="fill"
+          loading="lazy"
+        />
+        <blockquote
+          className="my-auto col-md-8"
+          cite="https://www.lefrasi.com/frase/victor-hugo-quasi-tutto-segreto-delle-anime-grandi?bg=p42"
+        >
+          <p lang="it" className="citation">{`"${citation}"`}</p>
+        </blockquote>
+      </div>
     </div>
   </Layout>
 );
