626463e6d9134d85b0ca275a384a422852b4be9b
Niccolò Mantovani <nicomanto49@gmail.com>
2021-05-10 17:19:15 +0200

    close #6

--- components/Modal/ModalPhoto.tsx
+++ components/Modal/ModalPhoto.tsx
@@ -12,25 +12,25 @@ type Props = {
 };
 
 const ModalPhoto = ({ show, onHide, indexImageShow, setIndexImageShow, items }: Props) => (
-    <Modal
-      show={show}
-      onHide={onHide}
-      size="xl"
-      tabIndex="-1"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title>{`Photographer: ${items[indexImageShow].ph}`}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="text-center">
-        <CarouselPhoto
-          indexImageActive={indexImageShow}
-          setIndexImageActive={setIndexImageShow}
-          items={items}
-        />
-      </Modal.Body>
-    </Modal>
+  <Modal
+    show={show}
+    onHide={onHide}
+    size="xl"
+    tabIndex="-1"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+  >
+    <Modal.Header closeButton>
+      <Modal.Title>{`Photographer: ${items[indexImageShow].ph}`}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body className="text-center">
+      <CarouselPhoto
+        indexImageActive={indexImageShow}
+        setIndexImageActive={setIndexImageShow}
+        items={items}
+      />
+    </Modal.Body>
+  </Modal>
 );
 
 export default ModalPhoto;
