657a0fe682d8587fbeb46f04c781a08b76693dd9
Niccolò Mantovani <nicomanto49@gmail.com>
2021-05-13 10:32:03 +0200

    Test resize font and image index

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,22 +14,25 @@ type Props = {
 
 const IndexPage = ({ title, subtitle, cit, photos }: Props) => (
   <Layout title="Home">
-    <h1 lang="it" className="display-3 mx-2 text-center title-home">
+    <h1 lang="it" className="display-4 mx-2 text-center title-home">
       {title}
     </h1>
     <h2 className="display-4 mx-2 text-center subtitle-home">{subtitle}</h2>
     <div className="py-5 px-5">
       <div className="row text-center">
-        <Image
-          cloudName="dszun6oiu"
-          className="photoPresentation col-md-4 py-2"
-          alt=""
-          publicId={photos[0].publicId}
-          width="500"
-          height="500"
-          crop="fill"
-          loading="lazy"
-        />
+        <div className="col-md-4">
+          <Image
+            cloudName="dszun6oiu"
+            className="photoPresentation"
+            alt=""
+            publicId={photos[0].publicId}
+            width="500"
+            height="500"
+            crop="fill"
+            loading="lazy"
+          />
+        </div>
+
         <div className="my-auto col-md-8 citation">
           <blockquote cite="https://www.goodreads.com/quotes/tag/modeling">
             <p>{`"${cit.citation}"`}</p>
