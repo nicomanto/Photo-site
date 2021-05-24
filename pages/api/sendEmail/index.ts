2d6ad5add64cffc72651cf2f402d966f16e6b8f4
Niccolò Mantovani <nicomanto49@gmail.com>
2021-05-24 18:47:14 +0200

    check in sendEmail data from request

--- pages/api/sendEmail/index.ts
+++ pages/api/sendEmail/index.ts
@@ -1,11 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { EmailInfo } from "../../../interfaces/Email";
 import sendCollaborationEmail from "../../../service/Nodemailer/manageEmail/collaborationEmail";
 
 const sendCollaboration = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (!req.body) {
-    res.status(401).json("Access denied");
+  if (!req.body.name) {
+    res.status(400).json("Name not found");
+  } else if (!req.body.surname) {
+    res.status(400).json("Surname not found");
+  } else if (!req.body.email) {
+    res.status(400).json("Email not found");
+  } else if (!req.body.message) {
+    res.status(400).json("Message not found");
   } else {
-    const isSent = await sendCollaborationEmail(req.body);
+    const infoEmail: EmailInfo = {
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      phone: req.body.phone,
+      message: req.body.message,
+    };
+
+    const isSent = await sendCollaborationEmail(infoEmail);
 
     if (isSent) {
       res.status(200).json({ message: "Email sent correctly" });
